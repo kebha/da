bdc60b40c5053a4cd2d8c0b70921054740613d9e
Jerin Kebha <kebhajerin@gmail.com>
2023-01-10 17:21:14 +1100

    added registration api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,23 +5,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-	[ApiController]
-	[Route("api/[controller]")]
-	public class UsersController
+	public class UsersController : BaseApiController
 	{
 		private readonly DataContext _context;
 
 		public UsersController(DataContext context)
 		{
 			_context = context;
-
 		}
 
 		[HttpGet]
 		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
 		{
 			return await _context.Users.ToListAsync();
-
 		}
 
 		[HttpGet("{id}")]
