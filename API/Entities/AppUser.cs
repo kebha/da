86ac456ef31ca4a357d9a0a47b8394b801ddcab4
Jerin Kebha <kebhajerin@gmail.com>
2023-01-12 23:27:32 +1100

    implemented repository pattern

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -11,7 +11,7 @@ namespace API.Entities
 		public DateOnly DateOfBirth { get; set; }
 		public string KnownAs { get; set; }
 		public DateTime Created { get; set; } = DateTime.UtcNow;
-		public DateTime LastActive { get; set; }
+		public DateTime LastActive { get; set; } = DateTime.UtcNow;
 		public string Gender { get; set; }
 		public string Introduction { get; set; }
 		public string LookingFor { get; set; }
@@ -20,9 +20,9 @@ namespace API.Entities
 		public string Country { get; set; }
 		public List<Photo> Photos { get; set; } = new();
 
-		public int GetAge()
-		{
-			return DateOfBirth.CalculateAge();
-		}
+		// public int GetAge()
+		// {
+		// 	return DateOfBirth.CalculateAge();
+		// }
 	}
 }
